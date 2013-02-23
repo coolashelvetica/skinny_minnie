90f0d2664b1e149017bd12b5f7bdec6a2f34b188
coolashelvetica <fara.ashiru@gmail.com>
2013-02-23 05:31:54 -0500

    Made Skinny Minnie Rails application into a git repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkinnyMinnie::Application.config.secret_token = 'e6b69cbe247f05e369e11b8e903c84a4cfca0d66169050739993c28668d194994d9ae68ea0a16be1c8845fbb65106bd72e1e35d86c7108147fa57fcb75554474'
